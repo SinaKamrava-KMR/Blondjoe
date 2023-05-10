464a8009a73b68b69b1690f7c52a29d26a1fbb14
Sina Kamrava <sinakamrava3118@gmail.com>
2023-05-10 11:21:21 +0330

    create all option of ui structure

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const { src, dest, watch, series } = require("gulp")
+const sass =require("gulp-sass")(require("sass"))
+
+function buildStyles() {
+  return src("./sass/*.scss")
+    .pipe(sass())
+    .pipe(dest("./src/css"))
+}
+
+
+function watchTask() {
+  watch(["./**/*.scss"],buildStyles)
+}
+
+
+exports.default = series(buildStyles,watchTask)
+
+
+
